29225788f02038f9611ad62f8a18a2fddbcd6411
huypqn <shuu.works@gmail.com>
2022-10-27 11:56:30 +0700

    feat: adding grid system

--- frontend/src/components/GlobalStyles/index.js
+++ frontend/src/components/GlobalStyles/index.js
@@ -1,5 +1,6 @@
 import './GlobalStyles.scss'
 import './MediaQuery.scss'
+import './Grid.scss'
 
 function GlobalStyles({ children }) {
     return children
--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,20 +1,6 @@
-import { useEffect } from 'react'
-
-const api = '/v1/api/user'
 
 function Home() {
-
-    useEffect(() => {
-        async function getData() {
-            const res = await fetch(api)
-            const data = await res.json()
-
-            console.log(data);
-        }
-
-        getData()
-    }, [])
-
+  
     return (
         <div>
             <h1>Home page</h1>
