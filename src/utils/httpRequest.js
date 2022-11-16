2701418fa7628c073d8b709d0ceb6d893005de9f
huypqn <shuu.works@gmail.com>
2022-11-16 12:02:55 +0700

    fix: cookie not saved in browser, search modal not align center

--- src/utils/httpRequest.js
+++ src/utils/httpRequest.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const request = axios.create({
-    baseURL: process.env.REACT_APP_BASE_URL
+    baseURL: process.env.REACT_APP_BASE_URL,
+    withCredentials: true
 })
 
 async function get(path, options = {}) {
